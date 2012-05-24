79a5485efc77307c1ee4cfc428ec4665fb25608d
Rohit Ashok Khot <rohitashokkhot@gmail.com>
2012-05-24 19:37:14 +0530

    added content editable support for text

--- js/script.js
+++ js/script.js
@@ -5,14 +5,37 @@ $(document).ready(function(){
 
 });
 
-$("#hlc h1").hover(function(){
+hedit= 0;
+$(".hoverable").hover(function(){
 	$(this).addClass("boundRect");
-	$(this).attr("contentEditable", "true");
-	$("#hrc").show();
+	//$(this).attr("contentEditable", "true");	
+	$(this).children(".hrc").append("<a href='#' id=hedit> Edit </a>");
+	$(this).children(".hrc").show();
 },function(){
 	$(this).removeClass("boundRect");
-	$(this).attr("contentEditable", "false");
-	$("#hrc").hide();
+	//$(this).attr("contentEditable", "false");
+	$(".hrc").hide();
+	$("#hedit").remove();
+});
+
+$("#hedit").live("click", function(){
+	var parent= $(this).parent().parent();
+	var editable = $(parent).find(".editable");
+	//var id = $(parent).attr("id");
+	//alert(id);
+	if(hedit==0)
+	{
+		$(editable).attr("contentEditable", "true");
+		$(editable).focus();
+		$(this).html("Save");
+		hedit=1;
+	}
+	else
+	{
+		$(editable).attr("contentEditable", "false");
+		$(this).html("Edit");
+		hedit=0;
+	}
 });
 
 
